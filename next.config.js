032bd3faeb4f940bba2c6184d5bac7f5c773dbec
HarukiHashimoto <coral.reef610@gmail.com>
2025-04-20 15:06:45 +0900

    Fix basePath to match repository name

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/coffee-timer',
+  basePath: '/CoffeeTimer',
   images: {
     unoptimized: true,
   },
