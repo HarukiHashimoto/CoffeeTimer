cf4ec112b8fffbfe8afba8baadeefeb82221e737
HarukiHashimoto <coral.reef610@gmail.com>
2025-04-20 14:32:07 +0900

    Add GitHub Pages configuration

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/coffee-timer',
+  images: {
+    unoptimized: true,
+  },
+}
+
+module.exports = nextConfig
