583764cf898e1ac61f4a44bb9fa2adb10d08c383
HarukiHashimoto <coral.reef610@gmail.com>
2025-04-29 14:59:46 +0900

    pwa

--- public/register-sw.js
+++ public/register-sw.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js').then((registration) => {
+    navigator.serviceWorker.register('/CoffeeTimer/sw.js').then((registration) => {
       console.log('ServiceWorker registration successful')
     }).catch((err) => {
       console.log('ServiceWorker registration failed: ', err)
