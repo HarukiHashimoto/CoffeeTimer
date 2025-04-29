61ca8864f0a0aeab53d5f4bb4d585f7e3fc9880f
HarukiHashimoto <coral.reef610@gmail.com>
2025-04-29 14:44:57 +0900

    pwa

--- next.config.js
+++ next.config.js
@@ -1,18 +1,3 @@
-const withPWA = require('@ducanh2912/next-pwa').default({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
-  buildExcludes: [/middleware-manifest\.json$/],
-  fallbacks: {
-    document: false,
-    image: false,
-    font: false,
-    audio: false,
-    video: false
-  }
-})
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
@@ -25,4 +10,4 @@ const nextConfig = {
   },
 }
 
-module.exports = withPWA(nextConfig)
+module.exports = nextConfig
